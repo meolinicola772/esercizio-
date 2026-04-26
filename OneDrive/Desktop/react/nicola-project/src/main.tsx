e64358c817aec733843b4960ff54e810a5493416
Nicola <meolinicola772@gmail.com>
2026-04-27 01:54:08 +0200

    app3

--- OneDrive/Desktop/react/nicola-project/src/main.tsx
+++ OneDrive/Desktop/react/nicola-project/src/main.tsx
@@ -1,52 +1,21 @@
+// Implementa un componente UncontrolledLogin che implementa tutte le operazioni del componente Login, 
+// ma utilizzando componenti non controllati.
+// Accedi ai valori del modulo utilizzando l'API FormData.
+//  Stampa i valori del modulo nella console quando il pulsante "login" viene cliccato.
 
-// In un'applicazione React, crea un function component Welcome che riceve una prop name e una prop age.
-// Crea un componente Age che riceve una prop age.
-// Il componente Welcome deve visualizzare un messaggio "Welcome, {name}!" all'interno di un tag <p> e 5 componenti Age.
-//  Per ogni rendering del componente Age, usa il rendering condizionale con una delle seguenti condizioni:
-// Il componente Age viene renderizzato solo se la prop age è maggiore di 18.
-// Il componente Age viene renderizzato solo se la prop age è presente.
-// Il componente Age viene renderizzato solo se la prop age è maggiore di 18 e minore di 65.
-// Il componente Age viene renderizzato solo se la prop age è maggiore di 18 e la prop name è uguale a "John".
-// Crea un nuovo componente Message che riceve una prop age e visualizza lo stesso messaggio se la prop è maggiore di 18;
-//  in caso contrario, visualizza il messaggio "You are very young!". Renderizza il componente Message all'interno del componente Welcome.
-
-
-import { AgeProps } from "./App";
-
-export function Age ({age}: AgeProps) {
-  return (
-    <>
-     <p>Your age is {age}</p>
-    </>
-  );
+export function UncontrolledLogin() {
+function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+event.preventDefault()
+const formData = new FormData(event.currentTarget)
+const username = formData.get('username')
+const password = formData.get('password')
+console.log(username, password)
 }
 
-
-export function Message({ age }: { age?: number }) {
-  return (
-    <p>{age && age > 18 ? "You are an adult!" : "You are very young!"}</p>
-  );
-}
-
-
-export function Welcome({ name = 'Nicola', age }: AgeProps) {
-  return (
-    <>
-      <p>Welcome, {name}!</p>
-
-      {age > 18 && <Age age={age} />}
-
-      {age && <Age age={age} />}
-
-      {age > 18 && age < 65 && <Age age={age} />}
-
-      {age > 18 && name === "John" && <Age age={age} />}
-
-      {age < 18 && <Age age={age} />}
-
-      {age !== undefined && age >= 65 && <Age age={age} />}
-
-      <Message age={age} />
-    </>
-  );
+return (
+<form onSubmit={handleFormSubmit}>
+<input name="username" />
+<input name="password" />
+<button type="submit">Login</button>
+</form>)
 }
