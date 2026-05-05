ba94c3e0bddef0a774811851a79b37a6378f4042
Nicola <meolinicola772@gmail.com>
2026-05-06 01:24:32 +0200

    useEffect

---
+++ esercizi/src/App.jsx
@@ -0,0 +1,26 @@
+// Crea un componente MouseClicker che contenga un pulsante con la prop name impostata alla stringa "one".
+//  Aggiungi un gestore di eventi al pulsante che stampa la prop name nella console quando cliccato, 
+//  leggendo la proprietà event.target.name.
+// Aggiungi un'immagine all'interno del pulsante e, senza creare un nuovo gestore di eventi o allegare
+//  un gestore di eventi all'immagine, stampa l'attributo name del pulsante indipendentemente dal fatto che si clicchi 
+//  sul pulsante o sull'immagine.
+// Aggiungi un gestore di eventi onClick all'immagine che stampa il src dell'immagine nella console. L'attributo name
+//  del pulsante viene comunque stampato nella console quando si clicca sull'immagine? Perché?
+// Come puoi impedire che l'attributo name del pulsante venga stampato nella console quando si clicca sull'immagine?
+// Crea un componente MultiButton con tre pulsanti, ciascuno con la prop name impostata rispettivamente a "one", "two"
+//  e "three". Aggiungi lo stesso gestore di eventi a ciascun pulsante che stampa la prop name nella console quando cliccato, 
+//  leggendo la proprietà event.target.name.
+
+
+
+import { useEffect, useState } from 'react'
+import { MouseClicker } from './components/click.jsx'
+
+
+export default function App() {
+  return (
+    <div>
+      <MouseClicker />
+    </div>
+  )
+}
--- esercizi/src/components/card.jsx
+++
@@ -1,9 +0,0 @@
-function Card ({nome,immagine,tipo,abilita}) {
-return (<div>
-    <h6>{nome}</h6>
-    <img src={immagine} alt={nome}/>
-    <ul>{tipo.map((x,i)=>(<li key={i}>{x.type.name}</li>))}</ul>
-    <ul>{abilita.map((x,i)=>(<li key={i}>{x.ability.name} </li>))}</ul>
-    </div>)
-}
-export default Card
