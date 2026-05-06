75e6133621f3747329c8b4c31ad39fbeb3cea00c
Nicola <meolinicola772@gmail.com>
2026-05-07 01:10:58 +0200

    nuovo commit

--- OneDrive/Desktop/react/nicola-project/src/App.tsx
+++ OneDrive/Desktop/react/nicola-project/src/App.tsx
@@ -1,15 +1,28 @@
 
-// Renderizza questo componente nel componente App, passando una prop name a tua scelta. Imposta un valore predefinito per la prop name.
-// Aggiungi una seconda prop chiamata age e visualizzala sotto il messaggio di benvenuto, all'interno di un tag <p> dopo il testo "Your age is ".
-// 4 .Modifica il valore passato alla prop name in modo che sia contenuto all'interno di un tag <strong>.
-// Estrai il messaggio "Your age is " in un nuovo componente chiamato Age e renderizzalo all'interno del componente Welcome. 
-// Passa al componente Age la prop age che Welcome sta ricevendo dal componente App.
+// Crea un componente MouseClicker che contenga un pulsante con la prop name impostata alla stringa "one".
+//  Aggiungi un gestore di eventi al pulsante che stampa la prop name nella console quando cliccato, 
+//  leggendo la proprietà event.target.name.
+// Aggiungi un'immagine all'interno del pulsante e, senza creare un nuovo gestore di eventi o allegare
+//  un gestore di eventi all'immagine, stampa l'attributo name del pulsante indipendentemente dal fatto che si clicchi 
+//  sul pulsante o sull'immagine.
+// Aggiungi un gestore di eventi onClick all'immagine che stampa il src dell'immagine nella console. L'attributo name
+//  del pulsante viene comunque stampato nella console quando si clicca sull'immagine? Perché?
+// Come puoi impedire che l'attributo name del pulsante venga stampato nella console quando si clicca sull'immagine?
+// Crea un componente MultiButton con tre pulsanti, ciascuno con la prop name impostata rispettivamente a "one", "two"
+//  e "three". Aggiungi lo stesso gestore di eventi a ciascun pulsante che stampa la prop name nella console quando cliccato, 
+//  leggendo la proprietà event.target.name.
 
 
-export type AgeProps={
-   name?: string
- age: number
+
+import { useEffect, useState } from 'react'
+import { MouseClicker } from './components/click.jsx'
+
+
+export default function App() {
+  return (
+    <div>
+      <MouseClicker />
+    </div>
+  )
 }
-
-
 
