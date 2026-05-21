b73eb349bd5acd72f6c0d5ed7e52b82cbdbafd5f
Nicola <meolinicola772@gmail.com>
2026-05-21 09:07:24 +0200

    nuovo

--- esercizi/src/assets/registrazione.jsx
+++
@@ -1,9 +0,0 @@
-// Crea un componente TodoList che renderizza un tag ul con un tag li per ogni elemento contenuto nella variabile di stato items.
-//  La variabile di stato items dovrebbe essere un array di stringhe. 
-//  Il componente TodoList dovrebbe anche contenere un tag input e un button. Quando il button viene cliccato, 
-//  l'event handler dovrebbe aggiungere il valore del tag input all'array items.
-// Modifica il componente TodoList in modo che l'input venga svuotato ogni volta che un Todo viene aggiunto all'array items.
-// Modifica il componente TodoList aggiungendo un button "reset" che svuota l'array items quando cliccato.
-// Modifica il componente TodoList aggiungendo un button "remove" a ogni tag li. Quando cliccato, 
-// l'event handler dovrebbe rimuovere l'elemento corrispondente dall'array items.
-
